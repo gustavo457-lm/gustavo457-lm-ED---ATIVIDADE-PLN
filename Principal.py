f839bfec88b5760e8cd438a033ba7e49b43c418c
Gustavo Lima Resende <joseresende333@gmail.com>
2026-08-24 20:48:32 -0300

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -1,3 +1,18 @@
-a = 5
-b = 2
-print(a+b)
+def main():
+    print("MENU")
+    print("1) Lê arquivo TXT e mostrar a quantidade de palavras")
+    print("2) Remover as stopwords e mostrar dados estatísticos")
+    print("3) Gerar e imprimir as top(k) palavras mais frequentes")
+    print("4) Gerar e imprimir os Hapaxes Legomenons " \
+            "(palavras de frequência única)")
+    print("5) Mostrar o gráfico com a Curva de Zipf e os " \
+            "cortes de Luhn.")
+    
+    escolha = int(input("Escolha uma das opções acima: "))
+    match (escolha):
+        case 1:
+
+        case 2:
+            
+
+main()
